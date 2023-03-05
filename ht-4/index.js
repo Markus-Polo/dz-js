4151cc45af20bff3515f6b1671ac7a7ec18a1a56
Mark Polosukhin <markipulooo@gmail.com>
2023-03-05 17:00:42 +0200

    fix

--- ht-4/index.js
+++ ht-4/index.js
@@ -1,18 +1,12 @@
-(() => {
-  const list = document.querySelectorAll('#tablist');
-  for(let i = 0; i < list.length; i++) {
-    list[i].addEventListener('click', (event) => {
-    const toggle = event.target.dataset.toggle;
-    const target = event.target.dataset.target;
-    const listName = list[i].dataset.number;
-    const elmentList = document.querySelector([listName]).children;
-      if(toggle === 'tab') {
-        for(let i = 0; i < elmentList.length; i++) {
-          elmentList[i].classList.remove("active");
-        }
-        element = document.querySelector([target]);
-        element.classList.add("active");
-      };
-    });
-  };
-})();
+function initTabs(){
+  const buttons = document.querySelectorAll(`[data-toggle='tab']`);
+  Array.from(buttons).forEach(button => {
+    button.addEventListener('click', function() {
+      const targetSelector = this.dataset.target;
+      const target = document.querySelector(targetSelector);
+      target.parentElement.querySelector('.active').classList.remove('active');
+      target.classList.add('active');
+    })
+  })
+};
+initTabs()
