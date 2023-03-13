bb2f7e190a1b69e21cdb0211b250750b457f3ceb
Mark Polosukhin <markipulooo@gmail.com>
2023-03-13 20:31:32 +0200

    fix

--- ht-6/index.js
+++ ht-6/index.js
@@ -1,38 +1,49 @@
 function initTooltip(){
-  const elments = Array.from(document.querySelectorAll(`[data-toggle="tooltip"]`));
-  elments.forEach(elment => {
+  const elements = Array.from(document.querySelectorAll(`[data-toggle="tooltip"]`));
+  elements.forEach(element => {
     const message = document.createElement('p');
-    elment.addEventListener('mouseover', () => {
-      const title = elment.getAttribute('title');
-      const placement = elment.dataset.placement;
-      const top = elment.getBoundingClientRect().top + window.pageYOffset;
-      const right = elment.getBoundingClientRect().right + window.pageXOffset;
-      const bottom = elment.getBoundingClientRect().bottom + window.pageYOffset;
-      const left = elment.getBoundingClientRect().left + window.pageXOffset;
-      const height = elment.getBoundingClientRect().height;
-      const width = elment.getBoundingClientRect().width;
+    element.addEventListener('mouseover', () => {
+      element.setAttribute('data-title', element.getAttribute('title'));
+      const title = element.dataset.title;
+      element.removeAttribute('title');
+      const placement = element.dataset.placement;
+      const top = element.getBoundingClientRect().top + window.pageYOffset;
+      const right = element.getBoundingClientRect().right + window.pageXOffset;
+      const bottom = element.getBoundingClientRect().bottom + window.pageYOffset;
+      const left = element.getBoundingClientRect().left + window.pageXOffset;
+      const height = element.getBoundingClientRect().height;
+      const width = element.getBoundingClientRect().width;
       message.classList.add('message');
-      message.innerHTML = `${title}`;
+      message.innerHTML = title;
       document.body.append(message);
-      if(placement === 'top') {
-        message.style.left = ((left + width/2) - message.getBoundingClientRect().width/2) + "px";
-        message.style.top = (top - (message.getBoundingClientRect().height + 15)) + "px";
-        message.classList.add('arrow-top')
-      } else if (placement === 'bottom') {
-        message.style.left = ((left + width/2) - message.getBoundingClientRect().width/2) + "px";
-        message.style.top = (bottom + 15) + "px";
-        message.classList.add('arrow-bottom');
-      } else if (placement === 'right') {
-        message.style.left = (right + 15) + "px";
-        message.style.top = ((top + height/2) - message.getBoundingClientRect().height/2) + "px";
-        message.classList.add('arrow-right');
-      } else if (placement === 'left') {
-        message.style.left = (left - (message.getBoundingClientRect().width + 15)) + "px";
-        message.style.top = ((top + height/2) - message.getBoundingClientRect().height/2) + "px";
-        message.classList.add('arrow-left');
+      const horizontalButtonCenter = left + width/2;
+      const verticalButtonCenter = top + height/2;
+      const horizontalMessageCenter = message.getBoundingClientRect().width/2;
+      const verticalMessageCenter = message.getBoundingClientRect().height/2;
+      switch (placement) {
+        case 'top':
+          message.style.left = (horizontalButtonCenter - horizontalMessageCenter) + "px";
+          message.style.top = (top - (message.getBoundingClientRect().height + 15)) + "px";
+          message.classList.add('arrow-top');
+          break;
+        case 'bottom':
+          message.style.left = (horizontalButtonCenter - horizontalMessageCenter) + "px";
+          message.style.top = (bottom + 15) + "px";
+          message.classList.add('arrow-bottom');
+          break;
+        case 'right':
+          message.style.left = (right + 15) + "px";
+          message.style.top = (verticalButtonCenter - verticalMessageCenter) + "px";
+          message.classList.add('arrow-right');
+          break;
+        case 'left':
+          message.style.left = (left - (message.getBoundingClientRect().width + 15)) + "px";
+          message.style.top = (verticalButtonCenter - verticalMessageCenter) + "px";
+          message.classList.add('arrow-left');
+          break;
       }
     })
-    elment.addEventListener('mouseout', () => {
+    element.addEventListener('mouseout', () => {
       message.remove();
     })
   })
