0368b3673909e454a2e00055c7a97698e668abac
Mark Polosukhin <markipulooo@gmail.com>
2023-03-18 16:39:22 +0200

    fix

--- ht-7/index.js
+++ ht-7/index.js
@@ -1,18 +1,16 @@
 function initCollapse(){
   const buttons = Array.from(document.querySelectorAll(`[data-toggle="collapse"]`));
-  let cardId;
   buttons.forEach(button => {
     button.addEventListener('click', () => {
-      cardId = button.getAttribute('href') || button.dataset.target;
-      collapseShow();
+      const cardId = button.getAttribute('href') || button.dataset.target;
+      collapseShow(cardId);
     })
   });
-  const collapseShow = () => {
+  const collapseShow = (cardId) => {
     const card = document.querySelector(cardId);
     if (card.clientHeight !== 0) {
       card.style.height = 0;
     } else {
-      console.log(card.scrollHeight)
       card.style.height = card.scrollHeight + 'px';
     }
   };
