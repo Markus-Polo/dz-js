c39a6dcdb739b3dd5d7e8296f057007051edc018
Mark Polosukhin <markipulooo@gmail.com>
2023-04-20 23:10:37 +0300

    fix

--- ht-12/index.js
+++ ht-12/index.js
@@ -1,6 +1,8 @@
 function carousel () {
   const nextButtons = Array.from(document.querySelectorAll(`[data-slide="next"]`));
   const prevButtons = Array.from(document.querySelectorAll(`[data-slide="prev"]`));
+  let directionPrev;
+  let directionNext;
   nextButtons.forEach(button => {
     button.addEventListener('click', () => {
       directionDetermination(button);
@@ -12,66 +14,50 @@ function carousel () {
     });
   });
   function directionDetermination(button) {
-    let direction;
     const buttonTarget = button.dataset.target;
     const buttonSlide = button.dataset.slide;
-    const carouselWrapper = document.querySelector(`${buttonTarget}`);
+    const carouselWrapper = document.querySelector(buttonTarget);
     const carouselItems = Array.from(carouselWrapper.querySelectorAll('.carousel-item'));
     if(buttonSlide === 'next') {
-      direction = true;
-      slide(carouselItems, direction);
+      directionPrev = 'left';
+      directionNext = 'right';
+      slide(carouselItems, buttonSlide);
     } else {
-      direction = false;
-      slide(carouselItems, direction);
+      directionPrev = 'right';
+      directionNext = 'left';
+      slide(carouselItems, buttonSlide);
     }
   };
-  function slide(carouselItems, direction) {
+  function slide(carouselItems, buttonSlide) {
     let nextItem;
-    let prevItem;
     let stop = false;
     for(let i = 0; stop === false; i++) {
-      if (direction === true && carouselItems.length - 1 !== i ) {
+      if (buttonSlide === 'next' && carouselItems.length - 1 !== i ) {
         nextItem = i + 1;
-      } else if (direction === true) {
+      } else if (buttonSlide === 'next') {
         nextItem = 0;
-      } else if (direction === false && i !== 0) {
-        prevItem = i - 1;
+      } else if (buttonSlide === 'prev' && i !== 0) {
+        nextItem = i - 1;
       } else {
-        prevItem = carouselItems.length - 1;
+        nextItem = carouselItems.length - 1;
       };
-      if (carouselItems[i].classList.contains('active') && direction === true) {
-        carouselItems[i].style.left='0';
+      if (carouselItems[i].classList.contains('active')) {
+        carouselItems[i].style[directionPrev]='0';
         carouselItems[nextItem].style.display='block';
-        carouselItems[nextItem].style.right='-100%';
+        carouselItems[nextItem].style[directionNext]='-100%';
         setTimeout(() => {
-          carouselItems[i].style.left='-100%';
-          carouselItems[nextItem].style.right='0';
+          carouselItems[i].style[directionPrev]='-100%';
+          carouselItems[nextItem].style[directionNext]='0';
         }, 10);
         setTimeout(() => {
           carouselItems[i].classList.remove('active');
           carouselItems[nextItem].classList.add('active');
-          carouselItems[i].style.left='';
+          carouselItems[i].style[directionPrev]='';
           carouselItems[nextItem].style.display='';
-          carouselItems[nextItem].style.right='';
+          carouselItems[nextItem].style[directionNext]='';
         }, 600);
         stop = true;
-      } else if (carouselItems[i].classList.contains('active') && direction === false) {
-        carouselItems[i].style.right='0';
-        carouselItems[prevItem].style.display='block';
-        carouselItems[prevItem].style.left='-100%';
-        setTimeout(() => {
-          carouselItems[i].style.right='-100%';
-          carouselItems[prevItem].style.left='0';
-        }, 10);
-        setTimeout(() => {
-          carouselItems[i].classList.remove('active');
-          carouselItems[prevItem].classList.add('active');
-          carouselItems[i].style.right='';
-          carouselItems[prevItem].style.display='';
-          carouselItems[prevItem].style.left='';
-        }, 600);
-        stop = true;
-      }
+      };
     };
   };
 };
