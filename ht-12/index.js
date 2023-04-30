15d5af1dd515f649db261d495e3732cbe64d0d37
Mark Polosukhin <markipulooo@gmail.com>
2023-04-30 23:11:41 +0300

    fix

--- ht-12/index.js
+++ ht-12/index.js
@@ -1,61 +1,72 @@
 function carousel () {
-  const nextButtons = Array.from(document.querySelectorAll(`[data-slide="next"]`));
-  const prevButtons = Array.from(document.querySelectorAll(`[data-slide="prev"]`));
-  let directionPrev;
-  let directionNext;
-  nextButtons.forEach(button => {
+  const buttons = Array.from(document.querySelectorAll('[data-slide="next"], [data-slide="prev"]'));
+  buttons.forEach(button => {
     button.addEventListener('click', () => {
       directionDetermination(button);
     });
   });
-  prevButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      directionDetermination(button);
-    });
-  });
+
   function directionDetermination(button) {
     const buttonTarget = button.dataset.target;
-    const buttonSlide = button.dataset.slide;
+    const direction = button.dataset.slide;
     const carouselWrapper = document.querySelector(buttonTarget);
     const carouselItems = Array.from(carouselWrapper.querySelectorAll('.carousel-item'));
-    if(buttonSlide === 'next') {
-      directionPrev = 'left';
-      directionNext = 'right';
-      slide(carouselItems, buttonSlide);
-    } else {
-      directionPrev = 'right';
-      directionNext = 'left';
-      slide(carouselItems, buttonSlide);
-    }
+      slide(carouselItems, direction);
   };
-  function slide(carouselItems, buttonSlide) {
+
+  function slide(carouselItems, direction) {
+    let directionPrev = 'left';
+    let directionNext = 'right';
     let nextItem;
     let stop = false;
-    for(let i = 0; stop === false; i++) {
-      if (buttonSlide === 'next' && carouselItems.length - 1 !== i ) {
+    let i = -1;
+    const timeForPrepare = 10;
+    const timeForSliding = 600;
+
+    const preparationForSlide = () => {
+      carouselItems[i].style[directionPrev]='0';
+      carouselItems[nextItem].style.display='block';
+      carouselItems[nextItem].style[directionNext]='-100%';
+    };
+
+    const sliding = () => {
+      setTimeout(() => {
+        carouselItems[i].style[directionPrev]='-100%';
+        carouselItems[nextItem].style[directionNext]='0';
+      }, timeForPrepare);
+    };
+
+    const completionSliding = () => {
+      setTimeout(() => {
+        carouselItems[i].classList.remove('active');
+        carouselItems[nextItem].classList.add('active');
+        carouselItems[i].style[directionPrev]='';
+        carouselItems[nextItem].style.display='';
+        carouselItems[nextItem].style[directionNext]='';
+      }, timeForSliding);
+    };
+
+    if (direction === 'prev') {
+      directionPrev = 'right';
+      directionNext = 'left';
+    };
+
+    while (stop === false) {
+      i++;
+      if (direction === 'next' && carouselItems.length - 1 !== i ) {
         nextItem = i + 1;
-      } else if (buttonSlide === 'next') {
+      } else if (direction === 'next') {
         nextItem = 0;
-      } else if (buttonSlide === 'prev' && i !== 0) {
+      } else if (direction === 'prev' && i !== 0) {
         nextItem = i - 1;
       } else {
         nextItem = carouselItems.length - 1;
       };
+
       if (carouselItems[i].classList.contains('active')) {
-        carouselItems[i].style[directionPrev]='0';
-        carouselItems[nextItem].style.display='block';
-        carouselItems[nextItem].style[directionNext]='-100%';
-        setTimeout(() => {
-          carouselItems[i].style[directionPrev]='-100%';
-          carouselItems[nextItem].style[directionNext]='0';
-        }, 10);
-        setTimeout(() => {
-          carouselItems[i].classList.remove('active');
-          carouselItems[nextItem].classList.add('active');
-          carouselItems[i].style[directionPrev]='';
-          carouselItems[nextItem].style.display='';
-          carouselItems[nextItem].style[directionNext]='';
-        }, 600);
+        preparationForSlide();
+        sliding();
+        completionSliding();
         stop = true;
       };
     };
